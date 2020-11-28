4015cca8d5278931e2fc4f444b79553b21e51c63
Lukas Tönne <lutnne@microsoft.com>
2020-11-28 08:56:13 +0100

    Whitespace cleanup.

--- Source/Galactitious/ProbabilityCurveFunctionLibrary.h
+++ Source/Galactitious/ProbabilityCurveFunctionLibrary.h
@@ -37,22 +37,22 @@ struct FDrawDebugCurveSettings
 UCLASS()
 class GALACTITIOUS_API UProbabilityCurveFunctionLibrary : public UBlueprintFunctionLibrary
 {
-    GENERATED_BODY()
+GENERATED_BODY()
 
-    UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
-    static UTexture2D* CurveToTexture2D(const FInterpCurveFloat& Curve, int32 Resolution);
+	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
+	static UTexture2D* CurveToTexture2D(const FInterpCurveFloat& Curve, int32 Resolution);
 
-    UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
-    static void ConvertCurveAsset(UCurveFloat *CurveAsset, FInterpCurveFloat& Curve);
+	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
+	static void ConvertCurveAsset(UCurveFloat *CurveAsset, FInterpCurveFloat& Curve);
 
-    UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
-    static void IntegrateCurve(const FInterpCurveFloat& Curve, float Offset, FInterpCurveFloat& IntegratedCurve);
+	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
+	static void IntegrateCurve(const FInterpCurveFloat& Curve, float Offset, FInterpCurveFloat& IntegratedCurve);
 
-    UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
-    static void NormalizeCurve(const FInterpCurveFloat& Curve, FInterpCurveFloat& NormalizedCurve);
+	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
+	static void NormalizeCurve(const FInterpCurveFloat& Curve, FInterpCurveFloat& NormalizedCurve);
 
-    UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
-    static void InvertCurve(const FInterpCurveFloat& Curve, FInterpCurveFloat& InvertedCurve);
+	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve)
+	static void InvertCurve(const FInterpCurveFloat& Curve, FInterpCurveFloat& InvertedCurve);
 
 	UFUNCTION(BlueprintCallable, Category = ProbabilityCurve, meta = (WorldContext = "WorldContextObject"))
 	static void DrawDebugCurve(
