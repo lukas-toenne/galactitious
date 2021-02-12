118d0f9fd4e82e7cb501e02e0ecc7f12c152e6f4
Lukas Tönne <lutnne@microsoft.com>
2021-02-12 14:59:19 +0000

    Reorganize and clean up widget files for the simulation UI.

--- Source/FastMultipoleSimulation/Public/FastMultipoleSimulationCache.h
+++ Source/FastMultipoleSimulation/Public/FastMultipoleSimulationCache.h
@@ -41,15 +41,20 @@ public:
 	UFastMultipoleSimulationCache();
 	virtual ~UFastMultipoleSimulationCache();
 
+	UFUNCTION(BlueprintGetter, Category = "Fast Multipole Simulation")
 	int32 GetCapacity() const;
+	UFUNCTION(BlueprintCallable, Category = "Fast Multipole Simulation")
 	void SetCapacity(int32 Capacity, EFastMultipoleCacheResizeMode ResizeMode);
 
+	UFUNCTION(BlueprintCallable, Category = "Fast Multipole Simulation")
 	int32 GetNumFrames() const;
 
 	FFastMultipoleSimulationFrame::ConstPtr GetFrame(int32 Step) const;
 	FFastMultipoleSimulationFrame::ConstPtr GetLastFrame() const;
 
+	UFUNCTION(BlueprintCallable, Category = "Fast Multipole Simulation")
 	void Reset();
+
 	bool AddFrame(const FFastMultipoleSimulationFrame::ConstPtr& InFrame);
 
 public:
@@ -60,6 +65,7 @@ public:
 	FFastMultipoleSimulationCacheFrameAddedDelegate OnFrameAdded;
 
 private:
+	UPROPERTY(EditAnywhere, BlueprintGetter = GetCapacity)
 	int32 Capacity = 32;
 
 	TArray<FFastMultipoleSimulationFrame::ConstPtr> Frames;
